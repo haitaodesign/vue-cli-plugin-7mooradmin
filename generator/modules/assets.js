7e08c486baf5ce723229c7349f7f9f9ba868bb6d
haitaodesign <15393243613@163.com>
2018-12-28 11:47:44 +0800

    fix template path

--- generator/modules/assets.js
+++ generator/modules/assets.js
@@ -1,7 +1,7 @@
 module.exports = (api) => {
   api.render({
-    './src/assets/images/logo.png': './templates/default/src/assets/images/logo.png',
-    './src/assets/styles/common.styl': './src/assets/styles/common.styl',
-    './src/assets/styles/variables.styl': './src/assets/styles/variables.styl'
+    './src/assets/images/logo.png': '../templates/default/src/assets/images/logo.png',
+    './src/assets/styles/common.styl': '../templates/default/src/assets/styles/common.styl',
+    './src/assets/styles/variables.styl': '../templates/default/src/assets/styles/variables.styl'
   })
 }
