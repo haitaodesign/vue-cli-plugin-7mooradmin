153a64390cf66482856db085dc2c8620515ff360
haitaodesign <15393243613@163.com>
2018-12-28 11:36:36 +0800

    fix images

--- generator/modules/assets.js
+++ generator/modules/assets.js
@@ -1,6 +1,6 @@
 module.exports = (api) => {
   api.render({
-    './src/assets/logo.png': './templates/default/src/assets/img/logo.png',
+    './src/assets/images/logo.png': './templates/default/src/assets/images/logo.png',
     './src/assets/styles/common.styl': './src/assets/styles/common.styl',
     './src/assets/styles/variables.styl': './src/assets/styles/variables.styl'
   })
