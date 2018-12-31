ddeae066e2aae2cc1423fb430125519bdd8c25d9
haitaodesign <15393243613@163.com>
2018-12-31 11:15:01 +0800

    add 组件模版

---
+++ generator/templates/default/src/components/index.js
@@ -0,0 +1,7 @@
+import AsideMenu from './aside-menu/index.js'
+import MoorBreadcrumb from './moor-breadcrumb/index.js'
+
+export {
+  AsideMenu,
+  MoorBreadcrumb
+}
---
+++ generator/templates/default/src/components/moor-breadcrumb/index.js
@@ -0,0 +1,6 @@
+import MoorBreadcrumb from './moor-breadcrumb.vue'
+
+MoorBreadcrumb.install = function (Vue) {
+  Vue.component(MoorBreadcrumb.name, MoorBreadcrumb)
+}
+export default MoorBreadcrumb
