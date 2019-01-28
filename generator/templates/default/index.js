76d54a624d84d86de6e8005544c23298d3952ccb
haitaodesign <15393243613@163.com>
2019-01-28 18:39:11 +0800

    feat: 新增模板分类

---
+++ generator/templates/default/index.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: lihaitao 
+ * @Date: 2019-01-28 17:48:25 
+ * @Last Modified by: lihaitao
+ * @Last Modified time: 2019-01-28 18:16:01
+ */
+// 如何组装拆封的模块
---
+++ generator/templates/modules/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: lihaitao 
+ * @Date: 2019-01-28 17:53:38 
+ * @Last Modified by: lihaitao
+ * @Last Modified time: 2019-01-28 17:56:11
+ */
+const NAMESPACE = 'moor-init-'
+ export default {
+   base: NAMESPACE+ 'base'
+ }
